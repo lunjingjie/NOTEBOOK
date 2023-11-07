494d603340849cf258b178d2bacdb1a4260ffe83
lunjingjie <lunjj@create80.com>
2023-11-07 17:32:25 +0800

    feat: update

---
+++ monitor/monitor-sdk/packages/browser/rollup.config.js
@@ -0,0 +1,12 @@
+import { umdPackage, iifePackage} from '../../rollup.base.config.js';
+
+// iife 增加自调用逻辑
+const footer = `if (window.__HEIMDALLR_OPTIONS__ && HEIMDALLR_BROWSER) {HEIMDALLR_BROWSER(window.__HEIMDALLR_OPTIONS__);}\n${iifePackage.output.footer}`;
+
+export default [umdPackage, {
+    ...iifePackage,
+    output: {
+        ...iifePackage.output,
+        footer
+    }
+}];
